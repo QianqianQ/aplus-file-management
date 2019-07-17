85bd26d18a8a263ede7c163b9dd1b8976371d4e8
QianqianQ <qianqian.qin@outlook.com>
2019-07-17 15:09:14 +0300

    Modify(add) functions:1. post files one by one, 2. post the zipped directory. Both are in-memory

---
+++ api.py
@@ -0,0 +1,79 @@
+import os
+import requests
+import subprocess
+from io import BytesIO
+import zipfile
+from requests_toolbelt import MultipartEncoder
+
+def upload_directory_one_by_one(directory):
+
+    headers = {
+                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
+              }
+
+    try:
+        for root, dirs, files in os.walk(directory):
+            for name in dirs:
+                # The relative path of the subdir (start from the uploaded_directory)
+                rel_dir = os.path.relpath(os.path.join(root, name), start=directory)
+
+            for name in files:
+                # Get the relative path of the file
+                rel_file_name = os.path.relpath(os.path.join(root, name), start=directory)
+
+                with open(os.path.join(root, name), 'rb') as f:
+                    encoder = MultipartEncoder(fields={'file_name': rel_file_name, 'file': f})
+                    headers['Content-Type'] = encoder.content_type
+                    requests.post(os.environ['PLUGIN_API'], data=encoder, headers=headers)
+        print('success!')
+    except Exception as e:
+        # If an error occurs, delete the whole new_directory from mooc-grader
+        encoder = MultipartEncoder({'error': repr(e)})
+        headers['Content-Type'] = encoder.content_type
+        response = requests.post(os.environ['PLUGIN_API'], headers=headers, data=encoder)
+
+
+def upload_direcotry_zipped(directory):
+
+    headers = {
+                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
+              }
+
+    buffer = BytesIO()
+
+    try:
+        with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zf:
+            for root, dirs, files in os.walk(directory):
+                for name in files:
+                    dest_file_name = os.path.relpath(os.path.join(root, name), start=directory)
+                    zf.write(os.path.join(root, name), dest_file_name)
+    except Exception as e:
+        raise repr(e)
+
+    buffer.seek(0)
+
+    # Check the content of the zip file
+    # with open("../test.zip",'wb') as f:
+    #     f.write(buffer.getvalue())
+
+    files = {'file': buffer.getvalue()}
+    response = requests.put(os.environ['PLUGIN_API'], headers=headers, files=files)
+    buffer.close()
+    print(response.text)
+
+
+def main():
+
+    os.environ['PLUGIN_API'] = 'http://0.0.0.0:8080/api/'
+    os.environ['PLUGIN_TOKEN'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJkZWZfY291cnNlIiwiaWF0IjoxNTYyODI4MzA0LCJpc3MiOiJzaGVwaGVyZCJ9.MUkoD27P6qZKKMM5juL0e0pZl8OVH6S17N_ZFzC7D0cwOgbcDaAO3S1BauXzhQOneChPs1KEzUxI2dVF-Od_gpN8_IJEnQnk25XmZYecfdoJ5ST-6YonVmUMzKP7UAcvzCFye7mkX7zJ1ADYtda57IUdyaLSPOWnFBSHX5B4XTzzPdVZu1xkRtb17nhA20SUg9gwCOPD6uLU4ml1aOPHBdiMLKz66inI8txPrRK57Gn33m8lVp0WTOOgLV5MkCIpkgVHBl50EHcQFA5KfPet3FBLjpp2I1yThQe_n1Zc6GdnR0v_nqX0JhmmDMOvJ5rhIHZ7B0hEtFy9rKUWOWfcug'
+
+    # if 'PLUGIN_API' in os.environ and 'PLUGIN_TOKEN' in os.environ:
+    #     print(os.environ['PLUGIN_API'])
+    #     print(os.environ['PLUGIN_TOKEN'])
+
+    upload_direcotry_zipped('/u/71/qinq1/unix/Desktop/my_new_course')
+    # upload_directory_one_by_one('/u/71/qinq1/unix/Desktop/my_new_course')
+
+
+if __name__ == "__main__":
+	main()
