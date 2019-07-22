8fdb29f0302ff99e2ee16f18a369cb47441d6187
QianqianQ <qianqian.qin@outlook.com>
2019-07-22 11:11:40 +0300

    Compressing course content as tar.gz format

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import requests
 import subprocess
 from io import BytesIO
 import zipfile
+import tarfile
 from requests_toolbelt import MultipartEncoder
 
 def upload_directory_one_by_one(directory):
@@ -154,6 +155,60 @@ def upload_yaml_direcotry_zipped(directory):
     print(response.text)
 
 
+def tar_add_bytes(tf, filename, bytestring):
+    """ Add a file to a tar archive
+    Args:
+        tf (tarfile.TarFile): tarfile to add the file to
+        filename (str): path within the tar file
+        bytestring (bytes or str): file contents. Must be :class:`bytes` or
+            ascii-encodable :class:`str`
+    
+    https://github.com/Autodesk/pyccc/blob/master/pyccc/docker_utils.py
+    """
+    if not isinstance(bytestring, bytes):  # it hasn't been encoded yet
+        bytestring = bytestring.encode('ascii')
+    buff = BytesIO(bytestring)
+    tarinfo = tarfile.TarInfo(filename)
+    tarinfo.size = len(bytestring)
+    tf.addfile(tarinfo, buff)
+
+
+def upload_yaml_direcotry_tar(directory):
+
+    build_dir = os.path.join(directory, '_build')
+    yaml_dir = os.path.join(build_dir, 'yaml')
+
+    if not os.path.exists(yaml_dir):
+         raise FileNotFoundError("No '_build/yaml' directory")
+    elif not os.path.isdir(yaml_dir):
+        raise NotADirectoryError("'_build/yaml' is not a directory")
+
+    headers = {
+                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
+              }
+
+    buffer = BytesIO()
+
+    try:
+        with tarfile.open(fileobj=buffer, mode='w:gz') as tf:
+            # Upload 'yaml' dir
+            for root, dirs, files in os.walk(yaml_dir):
+                for name in files:
+                    dest_file_name = os.path.relpath(os.path.join(root, name), start=yaml_dir)
+                    # tar_add_bytes(tf, dest_file_name, open(os.path.join(root, name),'rb').read())
+                    tf.add(os.path.join(root, name), dest_file_name)
+                
+    except Exception as e:
+        raise repr(e)
+
+    buffer.seek(0)
+
+    files = {'file': buffer.getvalue()}
+    response = requests.put(os.environ['PLUGIN_API'], headers=headers, files=files)
+    buffer.close()
+    print(response.text)
+
+
 def main():
 
     #os.environ['PLUGIN_API'] = 'http://0.0.0.0:8080/api/'
@@ -163,7 +218,8 @@ def main():
         print(os.environ['PLUGIN_API'])
         print(os.environ['PLUGIN_TOKEN'])
 
-    upload_yaml_direcotry_zipped(os.getcwd())
+    # upload_yaml_direcotry_zipped(os.getcwd())
+    upload_yaml_direcotry_tar(os.getcwd())
     # upload_yaml_direcotry_zipped_old(os.getcwd())
     # upload_direcotry_zipped(os.getcwd())
     
