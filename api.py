87d518a2d8803c59770a0f107b1a96e4b242d483
QianqianQ <qianqian.qin@outlook.com>
2019-07-22 11:53:52 +0300

    remove the function that sends zip-format compressed data to the api

--- api.py
+++ api.py
@@ -5,40 +5,6 @@ import tarfile
 import zipfile
 
 
-def upload_yaml_direcotry_zip(directory):
-
-    yaml_dir = os.path.join(directory, '_build', 'yaml')
-
-    if not os.path.exists(yaml_dir):
-         raise FileNotFoundError("No '_build/yaml' directory")
-    elif not os.path.isdir(yaml_dir):
-        raise NotADirectoryError("'_build/yaml' is not a directory")
-
-    headers = {
-                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
-              }
-
-    buffer = BytesIO()
-
-    try:
-        with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zf:
-
-            # Upload 'yaml' dir
-            for root, dirs, files in os.walk(yaml_dir):
-                for name in files:
-                    dest_file_name = os.path.relpath(os.path.join(root, name), start=yaml_dir)
-                    zf.write(os.path.join(root, name), dest_file_name)
-    except Exception as e:
-        raise repr(e)
-
-    buffer.seek(0)
-
-    files = {'file': buffer.getvalue()}
-    response = requests.put(os.environ['PLUGIN_API'], headers=headers, files=files)
-    buffer.close()
-    print(response.text)
-
-
 def upload_yaml_direcotry_tar(directory):
 
     build_dir = os.path.join(directory, '_build')
