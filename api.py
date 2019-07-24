cd064d2961a62d5ca0e091643486dff668ac6412
QianqianQ <qianqian.qin@outlook.com>
2019-07-24 16:59:13 +0300

    post compression file by chunks

--- api.py
+++ api.py
@@ -3,6 +3,26 @@ import requests
 from io import BytesIO
 import tarfile
 import zipfile
+from datetime import datetime
+import time
+
+def read_in_chunks(buffer, chunk_size=1024*1024*4):
+    while True:
+        data = buffer.read1(chunk_size)
+        if not data:
+            break
+        yield data
+
+def iter_read_chunks(buffer, chunk_size=1024*1024*4):
+    # Ensure it's an iterator and get the first field
+    it = iter(read_in_chunks(buffer,chunk_size))
+    prev = next(read_in_chunks(buffer,chunk_size))
+    for item in it:
+        # Lag by one item so I know I'm not at the end
+        yield prev, False
+        prev = item
+    # Last item
+    yield prev, True
 
 
 def upload_yaml_direcotry_tar(directory):
@@ -22,7 +42,8 @@ def upload_yaml_direcotry_tar(directory):
 
     # Add the JWT token to the request headers for the authentication purpose
     headers = {
-                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN'])
+                'Authorization': 'Bearer {}'.format(os.environ['PLUGIN_TOKEN']),
+                # 'Content-Type': 'multipart/form-data'
               }
 
     # Create the in-memory file-like object
@@ -36,17 +57,31 @@ def upload_yaml_direcotry_tar(directory):
                     dest_file_name = os.path.relpath(os.path.join(root, name), start=yaml_dir)
                     # Write the file to the in-memory tar
                     tf.add(os.path.join(root, name), dest_file_name)
-    except Exception as e:
-        raise repr(e)
+            creation_time = tf.tarinfo.mtime
+    except:
+        raise Exception('Error occurs!')
 
     # Change the stream position to the start
     buffer.seek(0)
 
-    # Upload the compressed file
-    files = {'file': buffer.getvalue()}
-    response = requests.put(os.environ['PLUGIN_API'], headers=headers, files=files)
-    buffer.close()
-    print(response.text)
+    # Upload the compressed file by chunks
+    index = 0
+    offset = 0
+    chunk_size = 1024*1024*4
+    for chunk, whether_last in iter_read_chunks(buffer,chunk_size=chunk_size):
+        offset = index + len(chunk)
+        headers['Content-Type'] = 'application/octet-stream'
+        headers['Chunk-Size'] = str(chunk_size)
+        headers['Chunk-Index'] = str(index)
+        headers['Chunk-Offset'] = str(offset)
+        if whether_last == True:
+            headers['Last-Chunk'] = 'True'
+        index = offset
+        try:
+            r = requests.put(os.environ['PLUGIN_API'], headers=headers, data=chunk)
+            print(r.text)
+        except:
+            raise Exception('Error occurs!')
 
 
 def main():
